ac3a26844c5cde24249b1a44f50557790c875ddd
Stanley Wang <wstan2001@gmail.com>
2020-09-19 15:06:08 -0400

    map of the US

--- app/static/map.js
+++ app/static/map.js
@@ -1,3 +1,4 @@
+  /*
   // initialize the map
   var map = L.map('map').setView([42.35, -71.08], 13);
 
@@ -7,6 +8,16 @@
     attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
     maxZoom: 17,
     minZoom: 9
-  }).addTo(map);
+  }).addTo(map);*/
 
-  
+  var map = L.map( 'map', {
+    center: [39, -95],
+    maxZoom: 17,
+    minZoom: 4,
+    zoom: 4
+});
+
+  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
