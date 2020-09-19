8121b065b52be8551b201010d5defcd8b27148dd
Stanley Wang <wstan2001@gmail.com>
2020-09-19 18:39:46 -0400

    updated location on click

--- app/static/postreq.js
+++ app/static/postreq.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $("#post_button").click(function() {
         console.log("clicked!");
+
+        //needs to send over relevant data to the database
+
+        //also needs to update marker popups
     });
 });
