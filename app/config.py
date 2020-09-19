35b63a512eb88771d8851aae6cffdbb0bf5151a7
Sirena Yu <yusirena4869@gmail.com>
2020-09-19 19:42:15 -0400

    some new commits for form

---
+++ app/config.py
@@ -0,0 +1,6 @@
+import os
+
+DEBUG = True
+
+base = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(base)
--- app/static/postreq.js
+++ app/static/postreq.js
@@ -1,6 +1,10 @@
+time_list = [];
+
 $(document).ready(function() {
-    $("#post_button").onclick(function() {
+    $("#post_button").click(function() {
         console.log("moo!");
+        var time = document.getElementById("wait_time").value;
+        console.log(time);
         document.getElementById("test").innerHTML = "You clicked it!";
     });
 });
