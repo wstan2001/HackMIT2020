b279df56620cf0e07093a0fac62e01a0788920e1
Dylan Liu <dylanliu@mit.edu>
2020-09-19 01:05:49 -0400

    donkey

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,21 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return "DONKEH"
+def poggers():
+    return render_template('index.html', title = 'COVID')
+
+@app.route('/donkey')
+def donkey():
+    return '''
+    <html>
+    <body>
+    <iframe width="1680" height="666" src="https://www.youtube.com/embed/fL-gKdENuMs" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </body>
+    </html>
+    
+    '''
+
+@app.route('/<word>')
+def lol(word):
+    return render_template('index.html', title=word.upper())
