9f5f2e8d95420820ff9a6bee43d6281f69f1b17a
Stanley Wang <wstan2001@gmail.com>
2020-09-19 15:42:37 -0400

    added markers, centered map on NC

--- app/static/map.js
+++ app/static/map.js
@@ -1,23 +1,19 @@
-  /*
-  // initialize the map
-  var map = L.map('map').setView([42.35, -71.08], 13);
-
-  // load a tile layer
-  L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-  {
-    attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-    maxZoom: 17,
-    minZoom: 9
-  }).addTo(map);*/
-
-  var map = L.map( 'map', {
-    center: [39, -95],
+var map = L.map( 'map', {
+    center: [35.75, -79],
     maxZoom: 17,
     minZoom: 4,
-    zoom: 4
+    zoom: 7
 });
 
-  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: ['a','b','c']
 }).addTo( map );
+
+
+//wow for some reason I don't need to load markers from the JSON file?
+for (var i = 0; i < markers.length; i++) {
+  L.marker( [markers[i].lat, markers[i].lng] )
+      .bindPopup( '<p">' + markers[i].name + '</p>' )
+      .addTo( map );
+}
