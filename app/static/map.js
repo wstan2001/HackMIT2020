31fddc2bc84df30dbed7eb56c5784a992835df00
Dylan Liu <dylanliu@mit.edu>
2020-09-20 02:14:34 -0400

    loads db data onto map upon visit

--- app/static/map.js
+++ app/static/map.js
@@ -14,19 +14,34 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var markers = new Array();
 function getData(data){
-
-  console.log(data);
-
+  data = JSON.parse(data)
 //wow for some reason I don't need to load markers from the JSON file?
 //let here is important, won't work if i is var
+
 for (let i = 0; i < locs.length; i++) {
-  var temp = L.marker( [locs[i].lat, locs[i].lng] )
-      .bindPopup( '<p>' + locs[i].name + '</p>' + 
-        '<p> Estimated wait time: ?? </p>' )
-          .on('click', function() {
-            console.log(locs[i].name);
-            $("#curr_loc").html(locs[i].name);
-  });
+  if (Object.keys(data).includes(locs[i].name)) {
+    timestamp = data[locs[i].name]['timestamp'];
+        var temp = L.marker( [locs[i].lat, locs[i].lng] )
+        .bindPopup( '<p>' + locs[i].name + '</p>' + 
+        '<p> Estimated wait time: '+data[locs[i].name]['latest']+ '</p>' +
+        '<p> Last Updated: ' + timestamp.substring(0,timestamp.length-4) + ' </p?')
+      .on('click', function() {
+        console.log(locs[i].name);
+        $("#curr_loc").html(locs[i].name);
+      });
+    }
+  
+     else  {
+
+      var temp = L.marker( [locs[i].lat, locs[i].lng] )
+          .bindPopup( '<p>' + locs[i].name + '</p>' + 
+            '<p> Estimated wait time: ?? </p>' )
+              .on('click', function() {
+                console.log(locs[i].name);
+                $("#curr_loc").html(locs[i].name);
+        });
+  }
+
   markers.push(temp);
   map.addLayer(temp);
   
